e92f6543a4ab7e63fd9af8c88e1381831ab3acf7
Ajay Saini <ajaysaini0505@gmail.com>
2024-12-26 14:43:25 +0530

    implemented addBook service

--- src/main/java/com/kata/Kata/Service/BookService.java
+++ src/main/java/com/kata/Kata/Service/BookService.java
@@ -1,6 +1,9 @@
 package com.kata.Kata.Service;
 
 import com.kata.Kata.Dto.BookDto;
+import com.kata.Kata.Model.Book;
+import com.kata.Kata.Repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,8 +12,26 @@ import java.util.List;
 @Service
 public class BookService {
 
-    public void addBook(BookDto book){
+    @Autowired
+    private BookRepository bookRepository;
 
+    private Book bookMapping(BookDto book){
+        Book newBook = new Book();
+        newBook.setId(book.getId());
+        newBook.setAuthor(book.getAuthor());
+        newBook.setTitle(book.getTitle());
+        newBook.setPublicationYear(book.getPublicationYear());
+        newBook.setIsAvailable(true);
+
+        return newBook;
+    }
+
+    public void addBook(BookDto book){
+        if(book.getId() == null){
+            return;
+        }
+
+        bookRepository.save(bookMapping(book));
     }
 
     public void borrowBook(String id){
