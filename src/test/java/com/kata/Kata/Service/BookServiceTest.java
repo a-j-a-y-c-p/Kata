0deb9d10d531ee7bed3bd4150dc818ad99aef447
Ajay Saini <ajaysaini0505@gmail.com>
2024-12-26 18:22:19 +0530

    implemented viewAvailableBook and returnBook tests

--- src/test/java/com/kata/Kata/Service/BookServiceTest.java
+++ src/test/java/com/kata/Kata/Service/BookServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -87,10 +88,55 @@ class BookServiceTest {
     }
 
     @Test
-    void returnBook() {
+    void returnBook_BookNotFound() {
+        String bookId = "1";
+        when(bookRepository.findById(bookId)).thenReturn(Optional.empty());
+
+        BookNotFoundException exception = assertThrows(BookNotFoundException.class, () -> bookService.returnBook(bookId));
+        assertEquals("Book with ID-1 is not found.", exception.getMessage());
     }
 
     @Test
-    void viewAvailableBook() {
+    void returnBook_BookWasNotBorrowed() {
+        String bookId = "1";
+        book.setIsAvailable(true);
+        when(bookRepository.findById(bookId)).thenReturn(Optional.of(book));
+
+        InvalidBookException exception = assertThrows(InvalidBookException.class, () -> bookService.returnBook(bookId));
+        assertEquals("Book with ID-1 was not borrowed.", exception.getMessage());
+    }
+
+    @Test
+    void returnBook_Returned() {
+        String bookId = "1";
+        book.setIsAvailable(false);
+        when(bookRepository.findById(bookId)).thenReturn(Optional.of(book));
+        when(bookRepository.save(book)).thenReturn(book);
+
+        bookService.returnBook(bookId);
+
+        assertTrue(book.getIsAvailable());
+        verify(bookRepository, times(1)).save(book);
+    }
+
+    @Test
+    void viewAvailableBook_BooksAreAvailable() {
+        List<BookDto> availableBooks = List.of(bookDto);
+        when(bookRepository.findByIsAvailableTrue()).thenReturn(availableBooks);
+
+        List<BookDto> result = bookService.viewAvailableBook();
+
+        assertNotNull(result);
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.size());
+        verify(bookRepository, times(1)).findByIsAvailableTrue();
+    }
+
+    @Test
+    void viewAvailableBook_NoBooksAvailable() {
+        when(bookRepository.findByIsAvailableTrue()).thenReturn(List.of());
+
+        BookNotFoundException exception = assertThrows(BookNotFoundException.class, () -> bookService.viewAvailableBook());
+        assertEquals("No book found.", exception.getMessage());
     }
 }
