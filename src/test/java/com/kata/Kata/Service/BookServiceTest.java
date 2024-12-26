28dbadc6c247de3ed6a400e0b8a3eabb4b39c9cf
Ajay Saini <ajaysaini0505@gmail.com>
2024-12-26 17:44:56 +0530

    implemented addBook tests

--- src/test/java/com/kata/Kata/Service/BookServiceTest.java
+++ src/test/java/com/kata/Kata/Service/BookServiceTest.java
@@ -1,15 +1,20 @@
 package com.kata.Kata.Service;
 
 import com.kata.Kata.Dto.BookDto;
+import com.kata.Kata.Exceptions.InvalidBookException;
 import com.kata.Kata.Model.Book;
 import com.kata.Kata.Repository.BookRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@SpringBootTest
 class BookServiceTest {
 
     @Mock
@@ -28,7 +33,21 @@ class BookServiceTest {
     }
 
     @Test
-    void addBook() {
+    void addBook_IdIsNotNull() {
+        when(bookRepository.save(Mockito.any(Book.class))).thenReturn(book);
+
+        bookService.addBook(bookDto);
+
+        verify(bookRepository, times(1)).save(Mockito.any(Book.class));
+    }
+
+    @Test
+    void addBook_IdIsNull() {
+        BookDto invalidBookDto = new BookDto(null, "Test Author", "Test Title", 2024);
+
+        InvalidBookException exception = assertThrows(InvalidBookException.class, () -> bookService.addBook(invalidBookDto));
+        assertEquals("Invalid book details.", exception.getMessage());
+        verify(bookRepository, times(0)).save(Mockito.any(Book.class));
     }
 
     @Test
