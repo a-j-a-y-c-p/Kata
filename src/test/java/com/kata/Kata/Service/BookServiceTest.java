a9a46483fc11d2e775e46d70de0be87f256dd518
Ajay Saini <ajaysaini0505@gmail.com>
2024-12-26 17:55:04 +0530

    implemented borrowBook tests

--- src/test/java/com/kata/Kata/Service/BookServiceTest.java
+++ src/test/java/com/kata/Kata/Service/BookServiceTest.java
@@ -1,6 +1,8 @@
 package com.kata.Kata.Service;
 
 import com.kata.Kata.Dto.BookDto;
+import com.kata.Kata.Exceptions.BookNotAvailableException;
+import com.kata.Kata.Exceptions.BookNotFoundException;
 import com.kata.Kata.Exceptions.InvalidBookException;
 import com.kata.Kata.Model.Book;
 import com.kata.Kata.Repository.BookRepository;
@@ -10,6 +12,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -51,7 +55,35 @@ class BookServiceTest {
     }
 
     @Test
-    void borrowBook() {
+    void borrowBook_Borrowed() {
+        String bookId = "1";
+        book.setIsAvailable(true);
+        when(bookRepository.findById(bookId)).thenReturn(Optional.of(book));
+        when(bookRepository.save(book)).thenReturn(book);
+
+        bookService.borrowBook(bookId);
+
+        assertFalse(book.getIsAvailable());
+        verify(bookRepository, times(1)).save(book);
+    }
+
+    @Test
+    void borrowBook_BookNotFound() {
+        String bookId = "1";
+        when(bookRepository.findById(bookId)).thenReturn(Optional.empty());
+
+        BookNotFoundException exception = assertThrows(BookNotFoundException.class, () -> bookService.borrowBook(bookId));
+        assertEquals("Book with ID-1 is not found.", exception.getMessage());
+    }
+
+    @Test
+    void borrowBook_BookIsNotAvailable() {
+        String bookId = "1";
+        book.setIsAvailable(false);
+        when(bookRepository.findById(bookId)).thenReturn(Optional.of(book));
+
+        BookNotAvailableException exception = assertThrows(BookNotAvailableException.class, () -> bookService.borrowBook(bookId));
+        assertEquals("Book with ID-1 is already borrowed.", exception.getMessage());
     }
 
     @Test
