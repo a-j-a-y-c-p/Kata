b5dfc88614849015916dac5263f11d4b00457d3b
Ajay Saini <ajaysaini0505@gmail.com>
2024-12-26 15:12:32 +0530

    implemented bookController

--- src/main/java/com/kata/Kata/Controller/BookController.java
+++ src/main/java/com/kata/Kata/Controller/BookController.java
@@ -1,9 +1,36 @@
 package com.kata.Kata.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kata.Kata.Dto.BookDto;
+import com.kata.Kata.Service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/library")
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/")
+    public List<BookDto> viewAvailableBooks(){
+        return new ArrayList<BookDto>(bookService.viewAvailableBook());
+    }
+
+    @PostMapping("/")
+    public void addBook(@RequestBody BookDto book){
+        bookService.addBook(book);
+    }
+
+    @PostMapping("/borrow/{id}")
+    public void borrowBook(@PathVariable String id){
+        bookService.borrowBook(id);
+    }
+
+    @PostMapping("/return/{id}")
+    public void returnBook(@PathVariable String id){
+        bookService.returnBook(id);
+    }
 }
